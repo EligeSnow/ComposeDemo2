e4c32394db0ca5fb8482f97df3358a5147d6257d
EligeSnow <ilyaklobertants398@gmail.com>
2026-09-14 14:35:14 +0700

    Delete template

--- app/src/main/java/com/example/composedemo2/MainActivity.kt
+++ app/src/main/java/com/example/composedemo2/MainActivity.kt
@@ -20,30 +20,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeDemo2Theme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true, showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    ComposeDemo2Theme {
-        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            Greeting("Compose", modifier = Modifier.padding(innerPadding))
-        }
-    }
-}
